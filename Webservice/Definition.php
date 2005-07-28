c980a55f40fc136b9768087082230dc9b92691da
Philippe Jausions <jausions@php.net>
2005-07-28 19:01:35 +0000

    Fixed typo in error message
    git-svn-id: http://svn.php.net/repository/pear/packages/Services_Webservice/trunk@191760 c90b9560-bf6c-de11-be94-00142212c4b1

--- Webservice/Definition.php
+++ Webservice/Definition.php
@@ -330,7 +330,7 @@ class Services_Webservice_Definition
                 preg_match_all('~@param\s(\S+)~', $docComments, $param);
                 $params = $method->getParameters();
                 if (count($params) !=  count($param)) {
-                    throw new Services_Webservice_Definition_IncompleteDocCommentException('Docblock comment don\'t match ' . $this->_classname . '::' . $methodName . '() signature.');
+                    throw new Services_Webservice_Definition_IncompleteDocCommentException('Docblock comment doesn\'t match ' . $this->_classname . '::' . $methodName . '() signature.');
                 }
                 for ($i = 0; $i < count($params); ++$i) {
                     $_class = $params[$i]->getClass();
