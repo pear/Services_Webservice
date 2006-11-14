2e2daf69eec5df4dc26aa69ea7d993e9153c76da
Manfred Weber <crafics@php.net>
2006-11-14 22:17:45 +0000

    A simple test
    git-svn-id: http://svn.php.net/repository/pear/packages/Services_Webservice/trunk@223354 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ tests/test_02.php
@@ -0,0 +1,25 @@
+<?php
+include_once('Services/Webservice.php');
+
+class myService extends Services_Webservice
+{
+	/**
+	* Says "Hello!"
+	*
+	* @param int
+	* @return string
+	*/
+	public function hello($i )
+	{
+		//create some logic here
+		return 'myString';
+	}
+}
+
+$myService = new myService(
+	'myService',
+	'example webservice description',
+	array('uri' => 'myService', 'encoding' => SOAP_ENCODED,'soap_version' => SOAP_1_2)
+);
+$myService->handle(); 
+?>
